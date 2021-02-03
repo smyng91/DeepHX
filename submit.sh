8b7c047e33d3d95f5c4729b5f9885c005611fa98
Yang <smyng@gatech.edu>
2021-02-02 20:51:31 -0700

    results

---
+++ submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J DeepHX
+#SBATCH --time=12:00:00
+#SBATCH -A ldrd
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --gpus-per-node=a100:4
+#SBATCH --mail-type="END"
+
+module load tensorflow
+
+python HX.py
+
+
