62d7adf102d34d4300d872c5571c64db3d277bf1
Yang <smyng@gatech.edu>
2021-02-26 21:20:04 -0700

    updated

--- submit.sh
+++ submit.sh
@@ -2,13 +2,13 @@
 #SBATCH -J DeepHX
 #SBATCH --time=12:00:00
 #SBATCH -A ldrd
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 #SBATCH --gpus-per-node=a100:4
 #SBATCH --mail-type="END"
 
 module load horovod
 
-python HX.py
+python NavierStokes_2D.py
 
 
